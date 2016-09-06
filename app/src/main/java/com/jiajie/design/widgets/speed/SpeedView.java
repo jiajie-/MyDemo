216586c2ffc073199054bb3799f0725767231d93
JiaJie Ruan <jiajie.ruan@gexne.com>
2016-09-06 10:26:32 +0800

    add some comments

--- app/src/main/java/com/jiajie/design/widgets/speed/SpeedView.java
+++ app/src/main/java/com/jiajie/design/widgets/speed/SpeedView.java
@@ -42,10 +42,12 @@ abstract public class SpeedView extends View {
     protected Bitmap speedViewBitmap;
     protected Paint speedViewBitmapPaint;
 
+    //一般在直接New一个View的时候调用。
     public SpeedView(Context context) {
         super(context);
     }
 
+    //一般在layout文件中使用的时候会调用，关于它的所有属性(包括自定义属性)都会包含在attrs中传递进来。
     public SpeedView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -80,12 +82,27 @@ abstract public class SpeedView extends View {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
+        super.onLayout(changed, left, top, right, bottom);//这里不需要，因为只是一个view
+        /**
+         * 确定布局的函数是onLayout，它用于确定子View的位置，在自定义ViewGroup中会用到，他调用的是子View的layout函数。
+         * 在自定义ViewGroup中，onLayout一般是循环取出子View，然后经过计算得出各个子View位置的坐标值，然后用以下函数设置子View位置。
+         * child.layout(l, t, r, b);
+         */
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        /**
+         * Q: 为什么要测量View大小？
+         * A: View的大小不仅由自身所决定，同时也会受到父控件的影响，为了我们的控件能更好的适应各种情况，一般会自己进行测量。
+         * 测量View大小使用的是onMeasure函数，我们可以从onMeasure的两个参数中取出宽高的相关数据：
+         * 从上面可以看出 onMeasure 函数中有 widthMeasureSpec 和 heightMeasureSpec 这两个 int 类型的参数， 毫无疑问他们是和宽高相关的， 但它们其实不是宽和高， 而是由宽、高和各自方向上对应的测量模式来合成的一个值：
+         * 测量模式一共有三种， 被定义在 Android 中的 View 类的一个内部类View.MeasureSpec中：
+         * 如果对View的宽高进行修改了，不要调用super.onMeasure(widthMeasureSpec,heightMeasureSpec);要调用setMeasuredDimension(widthsize,heightsize); 这个函数。
+         */
+        int widthmode = MeasureSpec.getMode(widthMeasureSpec);      //取出宽度的测量模式
+        int heightmode = MeasureSpec.getMode(heightMeasureSpec);    //取出高度的测量模式
         int width = getMeasuredWidth();
         int height = getMeasuredHeight();
         int size = (width > height) ? height : width;
@@ -95,6 +112,13 @@ abstract public class SpeedView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        /**
+         * 这个函数在视图大小发生改变时调用。
+         Q: 在测量完View并使用setMeasuredDimension函数之后View的大小基本上已经确定了，那么为什么还要再次确定View的大小呢？
+
+         A: 这是因为View的大小不仅由View本身控制，而且受父控件的影响，所以我们在确定View大小的时候最好使用系统提供的onSizeChanged回调函数。
+         *
+         */
         //refresh draw parameters
         refreshDraw();
         //calculate size change
